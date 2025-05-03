de1a3431bd9adec19591a0c4554c9ab8092b1f01
Shuaiwei Yu <yu.shuaiwei.1999@gmail.com>
2025-05-03 16:56:51 +0200

    delete the mono-service and replace them micro-services

---
+++ server/AttractionService/src/test/java/de/tum/attractionservice/AttractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tum.attractionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
