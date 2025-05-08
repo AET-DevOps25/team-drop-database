3fea63ed59090173ee4e76658e5c27be19d4e459
Shuaiwei Yu <yu.shuaiwei.1999@gmail.com>
2025-05-08 21:20:13 +0200

    setup

---
+++ server/AttractionService/src/main/java/de/tum/attractionservice/connection/ConnectionController.java
@@ -0,0 +1,31 @@
+package de.tum.attractionservice.connection;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/connection")
+public class ConnectionController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> checkConnection() {
+        return ResponseEntity.ok("Pong");
+    }
+
+    @GetMapping("/user-ping")
+    public ResponseEntity<String> checkUserConnection() {
+        return ResponseEntity.ok("User Pong");
+    }
+
+    @GetMapping("/admin-ping")
+    public ResponseEntity<String> checkAdminConnection() {
+        return ResponseEntity.ok("Admin Pong");
+    }
+
+    @GetMapping("/manager-ping")
+    public ResponseEntity<String> checkMangerConnection() {
+        return ResponseEntity.ok("Manager Pong");
+    }
+}
