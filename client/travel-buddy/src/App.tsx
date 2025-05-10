49a775042a6f438979692821d5568d790835f98b
Shuaiwei Yu <yu.shuaiwei.1999@gmail.com>
2025-05-10 14:03:19 +0200

    router configuration implemented

--- client/travel-buddy/src/App.tsx
+++ client/travel-buddy/src/App.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TravelBuddyAppBar from "./component/TravelBuddyAppBar";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <TravelBuddyAppBar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <TravelBuddyAppBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
