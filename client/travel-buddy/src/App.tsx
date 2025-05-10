134a4af936c61529dda28c89ba4f85e3ba853475
Shuaiwei Yu <yu.shuaiwei.1999@gmail.com>
2025-05-10 13:52:48 +0200

    react project set up

--- client/travel-buddy/src/App.tsx
+++ client/travel-buddy/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TravelBuddyAppBar from "./component/TravelBuddyAppBar";
 
 function App() {
   return (
     <div className="App">
+      <TravelBuddyAppBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
