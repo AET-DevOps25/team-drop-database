1238102f24cebf115888c0bb39ecdf5c48e90c30
niko <nzy612@icloud.com>
2025-05-19 12:21:13 +0200

    restructured code

--- server/UserService/src/main/java/de/tum/userservice/profile/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package de.tum.userservice.profile;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
-    ProfileEntity findByEmail(String email);
-}
