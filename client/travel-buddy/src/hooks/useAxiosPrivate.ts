15464dab966561a36e8442dd72acec129a1e9d5b
shuaiwei_yu <yu.shuaiwei.1999@gmail.com>
2025-06-13 14:55:49 +0200

    chat with interceptor implemented

--- client/travel-buddy/src/hooks/useAxiosPrivate.ts
+++ client/travel-buddy/src/hooks/useAxiosPrivate.ts
@@ -1,53 +1,32 @@
-import {useEffect} from "react";
-import { axiosPrivate } from "../api/axios";
-import useRefreshToken from "./useRefreshToken";
-import useAuth from "./useAuth";
-import {AxiosHeaders} from "axios";
+import { useEffect } from 'react';
+import { axiosAuth, axiosAttr, axiosUser } from '../api/axios';
+import setupInterceptors from './setupInterceptors';
+import useAuth from './useAuth';
+import useRefreshToken from './useRefreshToken';
 
 const useAxiosPrivate = () => {
+    const { auth } = useAuth();
     const refresh = useRefreshToken();
-    const {auth} = useAuth();
 
     useEffect(() => {
-        const requestIntercept = axiosPrivate.interceptors.request.use(
-            (config) => {
-                if (!config.headers) {
-                    config.headers = new AxiosHeaders();
-                }
+        const getToken = () => auth?.accessToken ?? null;
 
-                if (!config.headers.has("Authorization")) {
-                    config.headers.set("Authorization", `Bearer ${auth?.accessToken}`);
-                }
-
-                return config;
-            },
-            (error) => Promise.reject(error)
-        );
-
-        const responseIntercept = axiosPrivate.interceptors.response.use(
-            (response) => response,
-            async (error) => {
-                const prevRequest = error?.config;
-                if (error?.response?.status === 403 && !prevRequest?.sent) {
-                    prevRequest.sent = true;
-                    const newAccessToken = await refresh();
-                    prevRequest.headers = {
-                        ...prevRequest.headers,
-                        Authorization: `Bearer ${newAccessToken}`,
-                    };
-                    return axiosPrivate(prevRequest);
-                }
-                return Promise.reject(error);
-            }
-        );
+        const ejectors = [
+            // setupInterceptors(axiosAuth, refresh, getToken),
+            // setupInterceptors(axiosAttr, refresh, getToken),
+            setupInterceptors(axiosUser, refresh, getToken),
+        ];
 
         return () => {
-            axiosPrivate.interceptors.request.eject(requestIntercept);
-            axiosPrivate.interceptors.response.eject(responseIntercept);
+            ejectors.forEach(eject => eject());
         };
     }, [auth, refresh]);
 
-    return axiosPrivate;
+    return {
+        axiosAuth,
+        axiosAttr,
+        axiosUser,
+    };
 };
 
 export default useAxiosPrivate;
