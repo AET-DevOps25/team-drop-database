071b73bdae0314401dd4303430b4ec0cbdf70049
shuaiwei_yu <yu.shuaiwei.1999@gmail.com>
2025-06-13 15:45:43 +0200

    update frontend route as well as bruno file

--- client/travel-buddy/src/api/axios.ts
+++ client/travel-buddy/src/api/axios.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const BASE_AUTH_URL = 'http://localhost:8081/api/v1';
+const BASE_AUTH_URL = 'http://localhost:8081/auth-service/api/v1';
 
-const BASE_ATTR_URL = 'http://localhost:8082/api/v1';
+const BASE_ATTR_URL = 'http://localhost:8082/attraction-service/api/v1';
 
-const BASE_USER_URL = 'http://localhost:8083/api/v1';
+const BASE_USER_URL = 'http://localhost:8083/user-service/api/v1';
 
 export const axiosAuth = axios.create({
     baseURL: BASE_AUTH_URL,
