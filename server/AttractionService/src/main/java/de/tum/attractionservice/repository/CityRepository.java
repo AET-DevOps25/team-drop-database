f64c10f10a93b270984752ed200540f0bb62c81d
Haochuan Huai <haochuan.huai@tum.de>
2025-06-28 16:27:03 +0200

    adapted attraction endpoints

--- server/AttractionService/src/main/java/de/tum/attractionservice/repository/CityRepository.java
+++ server/AttractionService/src/main/java/de/tum/attractionservice/repository/CityRepository.java
@@ -1,9 +1,13 @@
 package de.tum.attractionservice.repository;
 
 import de.tum.attractionservice.model.CityEntity;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity, Long> {
+    Optional<CityEntity> findByName(String name);
 }
