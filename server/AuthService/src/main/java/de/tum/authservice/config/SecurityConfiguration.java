293fe07212bbebc01f8ec65335e70cf8aeacd274
Zhiyuan Ni <nzy612@icloud.com>
2025-07-14 10:59:01 +0200

    updated security configuration to allow prometheus connection established

--- server/AuthService/src/main/java/de/tum/authservice/config/SecurityConfiguration.java
+++ server/AuthService/src/main/java/de/tum/authservice/config/SecurityConfiguration.java
@@ -38,6 +38,8 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                            .requestMatchers(HttpMethod.GET,"/actuator/prometheus").permitAll()
+                            .requestMatchers(HttpMethod.GET,"/actuator/health", "/actuator/info").permitAll()
                             .requestMatchers(WHITE_LIST_URL).permitAll()
                             .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                             .requestMatchers("/connection/ping").permitAll()
